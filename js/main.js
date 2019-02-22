361fe0d0a228a0a2216d893420a38c9950b7c136
Favian-Ioel Poputa <favian.poputa@softvision.ro>
2019-02-22 18:35:25 +0200

    merge

--- js/main.js
+++ js/main.js
@@ -53,13 +53,6 @@
     skills += "<\/ul>";
     
     var about="";
-<<<<<<< HEAD
-    about += "<p>I can say I'm a very passionate guy ,<br>I love to learn new technology and share my knowledge with my friends.<br>I really think I still have more to learn and I think I will never abide at what I already know.<\/p><p>I want to join your team because I know it's a great place to enrich my knowledge, grow as a programmer and extend my area of expertize.<\/p>";
-
-    var about="<p>I can say I'm a very passionate guy ,<br>I love to learn new technology and share my knowledge with my friends.<br>I really think I still have more to learn and I think I will never abide at what I already know.<\/p><p>I want to join Endava because I know it's a great place to enrich my knowledge, grow as a programmer and extend my area of expertize.<\/p>";
-
-    var experience="<p><strong>Group Five Software<\/strong><br>";
-=======
     about += "<p>I can say I'm a very passionate guy ,<br>I love to learn new technology and share my knowledge with my friends.<br>I really think I still have more to learn and I think I will never abide at what I already know.<\/p>";
     var experience="";
     experience += "<p><strong>SoftVision<\/strong><br>";
@@ -68,7 +61,6 @@
     experience += "";
     experience += "";
     experience += "<p><strong>Group Five Software<\/strong><br>";
->>>>>>> 865bcf8b10c304e8b1b385174c80aeee9a71179c
     experience += "                <i>Junior PHP Developer<\/i><br>";
     experience += "                Febr 2016 – Aug 2017<\/p>";
     experience += "                <p>Group Five Software is a small company that develops big projects.<\/p>";
@@ -136,21 +128,7 @@
             
         }else if($scope.read.toLowerCase() == 'download'){
             $scope.array.push(['',$scope.read]);
-<<<<<<< HEAD
-            window.open('ioel.pdf', '_blank', 'fullscreen=yes');
-            history_commands.push($scope.read);
-            $scope.read = '';
-            
-        }else if($scope.read.toLowerCase() == 'history'){
-            var rett='';
-            for (var i=0;i<history_commands.length; i++) {
-                 rett+= '<p>'+i+'. '+history_commands[i]+'</p>';
-            }
-            $scope.array.push([rett==''?no_history:rett,$scope.read]);
-            history_commands.push($scope.read);
-=======
             window.open('CurriculumVitae.pdf', '_blank', 'fullscreen=yes');
->>>>>>> 865bcf8b10c304e8b1b385174c80aeee9a71179c
             $scope.read = '';
             
         }else {
